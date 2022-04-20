2e25c4379ef5ae64b11e7763c0f229739e34e39a
Valeriy Lazarev <v.lazarev.spb@gmail.com>
2022-04-20 09:48:06 +0300

    lesson3: implemented the movement of two independent ships with different speeds

--- core/src/ru/lazarev/game/Game.java
+++ core/src/ru/lazarev/game/Game.java
@@ -5,54 +5,45 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.Animation;
-import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-import com.badlogic.gdx.math.Circle;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Rectangle;
-import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.ScreenUtils;
 import ru.lazarev.game.animation.Explosion;
 import ru.lazarev.game.animation.MyAnimation;
+import ru.lazarev.game.sprites.SpaceShip;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ListIterator;
 
-import static ru.lazarev.game.utils.GfxUtils.getAngle;
 import static ru.lazarev.game.utils.GfxUtils.getPosition;
 
 public class Game extends ApplicationAdapter {
+    private static final int COUNT_SPACE_SHIPS = 2;
     private SpriteBatch batch;
     private ShapeRenderer shapeRenderer;
     private MyAnimation turretAnimation;
     private MyAnimation bodyAnimation;
     private MyAnimation headAnimation;
     private List<Explosion> explosions;
+
+    private List<SpaceShip> spaceShips;
     private TextureAtlas textureAtlas;
-    private Rectangle rectangle;
     private Rectangle mouseRectangle;
-    private Circle circle;
-
-    private float x;
-    private float y;
-//    private Sprite headSprite;
-
-
-    private int count;
+    private int numberOfHits;
 
     @Override
     public void create() {
-        x = Gdx.graphics.getWidth() + 100;
         textureAtlas = new TextureAtlas("img/atlas/main.atlas");
         batch = new SpriteBatch();
         shapeRenderer = new ShapeRenderer();
-        rectangle = new Rectangle(250, 250, 100, 200);
         mouseRectangle = new Rectangle();
-        circle = new Circle(250, 300, 50);
         explosions = new ArrayList<>();
+        spaceShips = new ArrayList<>();
+        fillSpaceShips();
         turretAnimation = new MyAnimation(textureAtlas.findRegion("turret-sprites-deployment"), Animation.PlayMode.NORMAL, 8, 1, 8);
         bodyAnimation = new MyAnimation(textureAtlas.findRegion("turret-sprites-body"), Animation.PlayMode.LOOP, 2, 1, 16);
         headAnimation = new MyAnimation(textureAtlas.findRegion("turret-sprites-head-shot-idle"), Animation.PlayMode.NORMAL, 5, 1, 60);
@@ -62,57 +53,37 @@ public class Game extends ApplicationAdapter {
     public void render() {
         ScreenUtils.clear(Color.FOREST);
         boolean fire = Gdx.input.isButtonPressed(Input.Buttons.LEFT);
-        turrelAttakHandling();
-        targetMouse();
-        ArrayList<Sprite> sprites = new ArrayList<>();
-        for (int i = 0; i < sprites.size(); i++) {
-            sprites.add(new Sprite());
+
+        getTurretAttackHandling();
+        getTargetMouse();
+        getSpaceShip();
+
+        for (SpaceShip spaceShip : spaceShips) {
+            hitHandling(fire, spaceShip);
+            if (spaceShip.isGoingOffScreen()) {
+                Gdx.app.exit();
+            }
         }
-        Sprite headSprite = createFlyngSprite();
-        flyingSprite(headSprite);
-
-        hitHandling(fire, headSprite);
     }
 
-    private Sprite createFlyngSprite() {
-        return new Sprite(textureAtlas.findRegion("msTurret"));
-    }
-
-    private void flyingSprite(Sprite headSprite) {
-        Vector2 headOrigin = new Vector2(headSprite.getWidth() / 2, headSprite.getRegionHeight() >> 1);
-        headSprite.setOrigin(headOrigin.x, headOrigin.y);
-        headSprite.setScale(1);
-        x -= 0.9f;
+    private void getSpaceShip() {
         batch.begin();
-        Vector2 headPosition = new Vector2(x, y);
-        headSprite.setPosition(headPosition.x - headOrigin.x, headPosition.y - headOrigin.y);
-        headSprite.setRotation(getAngle(headPosition) + 90);
-        headSprite.draw(batch);
+        for (SpaceShip spaceShip : spaceShips) {
+            spaceShip.render(batch);
+        }
         batch.end();
-
-        if (isGoingOffScreen(headPosition.x, headPosition.y)) {
-          Gdx.app.exit();
-         }
-        shapeRenderer.end();
-
-        shapeRenderer.begin(ShapeRenderer.ShapeType.Line);
-        shapeRenderer.rect(
-                headSprite.getBoundingRectangle().x,
-                headSprite.getBoundingRectangle().y,
-                headSprite.getBoundingRectangle().getWidth(),
-                headSprite.getBoundingRectangle().getHeight());
-        shapeRenderer.end();
     }
 
-    private void targetMouse() {
+    private void getTargetMouse() {
         shapeRenderer.begin(ShapeRenderer.ShapeType.Line);
         shapeRenderer.line(getPosition().x - 10, getPosition().y, getPosition().x + 10, getPosition().y);
         shapeRenderer.line(getPosition().x, getPosition().y - 10, getPosition().x, getPosition().y + 10);
         mouseRectangle.set(getPosition().x - 10, getPosition().y - 10, 20, 20);
         shapeRenderer.rect(mouseRectangle.x, mouseRectangle.y, mouseRectangle.getWidth(), mouseRectangle.getHeight());
+        shapeRenderer.end();
     }
 
-    private void turrelAttakHandling() {
+    private void getTurretAttackHandling() {
         float rotation = 360 - MathUtils.atan2(getPosition().x - 25, getPosition().y - 34) * MathUtils.radiansToDegrees;
         batch.begin();
         if (!turretAnimation.isFinished()) {
@@ -137,23 +108,26 @@ public class Game extends ApplicationAdapter {
         batch.end();
     }
 
-    private void hitHandling(boolean fire, Sprite headSprite) {
+    private void hitHandling(boolean fire, SpaceShip ship) {
         if ((fire & !headAnimation.isFinished()) || (!fire & !headAnimation.isFinished()))
             headAnimation.setTime(Gdx.graphics.getDeltaTime());
         if (fire & headAnimation.isFinished()) {
             headAnimation.resetTime();
             explosions.add(new Explosion(textureAtlas.findRegion("explosion"), Animation.PlayMode.NORMAL, 4, 4, 16, "audio/explosion.mp3"));
-            if (headSprite.getBoundingRectangle().contains(getPosition())) {
-                x = Gdx.graphics.getWidth() + 100;
-                y = MathUtils.random(0, Gdx.graphics.getHeight() - headSprite.getHeight());
-                count++;
+            if (ship.getSprite().getBoundingRectangle().contains(getPosition())) {
+                ship.setX(Gdx.graphics.getWidth() + 100);
+                ship.setY(MathUtils.random(0, Gdx.graphics.getHeight() - ship.getSprite().getHeight()));
+                ship.setSpeed(MathUtils.random(0.9f, 7.0f));
+                numberOfHits++;
             }
         }
-        Gdx.graphics.setTitle("Спрайтов подбито: " + String.valueOf(count));
+        Gdx.graphics.setTitle("Спрайтов подбито: " + numberOfHits);
     }
 
-    private boolean isGoingOffScreen(float x, float y) {
-        return x - 10 <= 0;
+    private void fillSpaceShips() {
+        for (int i = 0; i < COUNT_SPACE_SHIPS; i++) {
+            spaceShips.add(new SpaceShip());
+        }
     }
 
     @Override
---
+++ core/src/ru/lazarev/game/sprites/SpaceShip.java
@@ -0,0 +1,62 @@
+package ru.lazarev.game.sprites;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Vector2;
+
+import static ru.lazarev.game.utils.GfxUtils.getAngle;
+
+public class SpaceShip {
+
+    private final Sprite sprite;
+    private float x;
+    private float y;
+    private float speed;
+
+    public SpaceShip() {
+        TextureAtlas textureAtlas = new TextureAtlas("img/atlas/main.atlas");
+        this.sprite = new Sprite(textureAtlas.findRegion("msTurret"));
+        this.speed = MathUtils.random(0.9f, 5.0f);
+        x = Gdx.graphics.getWidth() + 100;
+        y = MathUtils.random(0, Gdx.graphics.getHeight() - sprite.getHeight());
+
+    }
+
+    public void setSpeed(float speed) {
+        this.speed = speed;
+    }
+
+    public Sprite getSprite() {
+        return sprite;
+    }
+
+    public void render(SpriteBatch batch) {
+        Vector2 headOrigin = new Vector2(sprite.getWidth() / 2, sprite.getRegionHeight() >> 1);
+        sprite.setOrigin(headOrigin.x, headOrigin.y);
+        sprite.setScale(1);
+        x -= speed;
+        Vector2 headPosition = new Vector2(x, y);
+        sprite.setPosition(headPosition.x - headOrigin.x, headPosition.y - headOrigin.y);
+        sprite.setRotation(getAngle(headPosition) + 90);
+        sprite.draw(batch);
+    }
+
+    private boolean isGoingOffScreen(float x) {
+        return x - 10 <= 0;
+    }
+
+    public boolean isGoingOffScreen() {
+        return isGoingOffScreen(this.x);
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+}
