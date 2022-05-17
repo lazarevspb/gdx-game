5b5d99c290abd5ee25c577ce01d7bd8d7b4ce26a
Valeriy Lazarev <v.lazarev.spb@gmail.com>
2022-05-17 19:37:20 +0300

    Lesson7

--- core/src/ru/lazarev/game/game_objects/BaseEnemy.java
+++ core/src/ru/lazarev/game/game_objects/BaseEnemy.java
@@ -6,22 +6,30 @@ import static ru.lazarev.game.utils.GfxUtils.getAngle;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
+import ru.lazarev.game.animation.MyAnimation;
 
 public class BaseEnemy {
 
+  public static final TextureAtlas MAIN_ATLAS = new TextureAtlas("img/atlas/main.atlas");
   protected Vector2 position;
   protected Vector2 rotate;
   protected Vector2 origine;
   protected float health;
   protected float speed;
   protected float damage;
+  protected float rotDif;
   protected Sprite sprite;
   protected boolean isDamaged;
+//  protected Sprite skin;
+
+  MyAnimation animation;
 
   public BaseEnemy(String name, float speed, float health) {
 
@@ -50,6 +58,23 @@ public class BaseEnemy {
     sprite.setOrigin(originX, originY);
 
   }
+
+  public BaseEnemy(String name, float speed, float health, int anmColumns, int anmLines, int anmFps){
+
+    isDamaged = false;
+    animation = new MyAnimation(MAIN_ATLAS.findRegion(name), Animation.PlayMode.NORMAL, anmColumns, anmLines, anmFps);
+    animation.setTime(0);
+    sprite = new Sprite(animation.getRegion());
+    position = new Vector2();
+    position.x = Gdx.graphics.getWidth();
+    position.y = MathUtils.random(Gdx.graphics.getHeight()/3, Gdx.graphics.getHeight()-sprite.getHeight());
+    sprite.setPosition(position.x, position.y);
+    sprite.setScale(1);
+    this.speed = speed;
+    this.health = health;
+    rotDif = 90;
+  }
+
 
   public float damage(float damage) {
     isDamaged = true;
@@ -95,5 +120,13 @@ public class BaseEnemy {
       sprite.setColor(Color.WHITE);
     }
     sprite.draw(batch);
+  }
+
+  public Sprite getSkin() {
+    return sprite;
+  }
+
+  public void setDif(float dif) {
+    this.rotDif = dif;
   }
 }
--- core/src/ru/lazarev/game/game_objects/BigShip.java
+++ core/src/ru/lazarev/game/game_objects/BigShip.java
@@ -2,6 +2,9 @@ package ru.lazarev.game.game_objects;
 
 import static ru.lazarev.game.utils.GfxUtils.getAngle;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 import java.util.ArrayList;
@@ -12,11 +15,38 @@ public class BigShip extends BaseEnemy {
   private final List<BaseEnemy> turrets;
   private int turretToDamage;
 
+
+
+  private Music music;
+
   public BigShip(String name, float speed, float health) {
     super(name, speed, health);
     turretToDamage = -1;
     turrets = new ArrayList<>();
     getAdditionalWeapons();
+  }
+
+  public BigShip(String name, float speed, float health, int column, int lines, int fps) {
+    super(name, speed, health, column, lines, fps);
+    music = Gdx.audio.newMusic(Gdx.files.internal("audio/explosion.mp3"));
+    turretToDamage = -1;
+    turrets = new ArrayList<>();
+    turrets.add(new BaseEnemy("msSmallGun", speed, 10, 4, 1, 16));
+    turrets.get(turrets.size()-1).setPosition(new Vector2(position.x+150.0f, position.y + (sprite.getHeight()-35.0f)));
+    turrets.get(turrets.size()-1).setOrigine(new Vector2(7, 12));
+    turrets.get(turrets.size()-1).setDif(0);
+    turrets.get(turrets.size()-1).setDamage(0.01f);
+
+    turrets.add(new BaseEnemy("msSmallGun", speed, 10, 4, 1, 16));
+    turrets.get(turrets.size()-1).setPosition(new Vector2(position.x+150.0f + 45, position.y + (sprite.getHeight()-35.0f)));
+    turrets.get(turrets.size()-1).setOrigine(new Vector2(7, 12));
+    turrets.get(turrets.size()-1).setDif(0);
+    turrets.get(turrets.size()-1).setDamage(0.01f);
+
+    turrets.add(new BaseEnemy("msTurret", speed, 10, 3, 1, 6));
+    turrets.get(turrets.size()-1).setPosition(new Vector2(position.x+150.0f + 80, position.y + (sprite.getHeight()-50.0f)));
+    turrets.get(turrets.size()-1).setOrigine(new Vector2(16, 21));
+    turrets.get(turrets.size()-1).setDamage(0.1f);
   }
 
   private void getAdditionalWeapons() {
